904796f336c0c33884a2981f661552d8ec09d2b5
Lim Woojin <95209789+woojin2296@users.noreply.github.com>
2024-10-11 00:34:06 +0000

    update ui

--- app/page.tsx
+++ app/page.tsx
@@ -4,53 +4,160 @@ import Graph from "@/components/Graph";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { extractFreezerData, extractRefrigeratorData } from "@/service/DataService";
 import { useEffect, useState } from "react";
+import { ThermometerSnowflake, Droplets } from 'lucide-react';
 
 export default function Home() {
   const [data, setData] = useState<any>([]);
 
-  useEffect(() => {
-    fetchData();
-
-    const updateData = setInterval(async () => {
-      fetchData();
-    }, 2000);
-
-    return () => {
-      clearInterval(updateData);
-    };
-  }, []);
-
-  function fetchData() {
-    fetch("/api/graphdata")
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-      });
-  }
 
   return (
-    <div className="flex bg-customDarkBg h-[calc(100vh-3.5rem)] p-2">
-      <Card className="bg-customDark h-2/6 w-1/2 m-2">
-        <CardHeader>
-          <CardTitle className="text-white">Freezer</CardTitle>
-          <CardDescription className="text-white">Current temperature and humidity of freezer.</CardDescription>
-        </CardHeader>
-        <CardContent className="flex">
-          <div className="flex flex-grow justify-center items-center text-6xl text-white">
-            25°C
-          </div>
-          <div className="flex-grow text-6xl text-white">
-            50%
-          </div>
-        </CardContent>
-      </Card>
-      <Card className="bg-customDark h-2/6 w-1/2 m-2">
-        <CardHeader className="text-white">
-          <CardTitle>Refrigerator</CardTitle>
-          <CardDescription className="text-white">Current temperature and humidity of refrigerator.</CardDescription>
-        </CardHeader>
-        <CardContent>test</CardContent>
-      </Card>
+    <div className="bg-customDarkBg h-[calc(100vh-3.5rem)] p-2">
+      <div className="flex h-2/6">
+        <Card className="flex flex-col bg-customDark w-1/2 m-2">
+          <CardHeader>
+            <CardTitle className="text-white">Freezer</CardTitle>
+            <CardDescription className="text-white">Current temperature and humidity of freezer.</CardDescription>
+          </CardHeader>
+          <CardContent className="flex felx-row flex-grow">
+            <div className="flex flex-grow justify-center items-center text-6xl text-white">
+              <ThermometerSnowflake className="h-12 w-12 mx-4"/> 25°C
+            </div>
+            <div className="flex flex-grow justify-center items-center text-6xl text-white">
+              <Droplets className="h-12 w-12 mx-4"/> 50%
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col bg-customDark w-1/2 m-2">
+          <CardHeader className="text-white">
+            <CardTitle>Refrigerator</CardTitle>
+            <CardDescription className="text-white">Current temperature and humidity of refrigerator.</CardDescription>
+          </CardHeader>
+          <CardContent className="flex felx-row flex-grow">
+            <div className="flex flex-grow justify-center items-center text-6xl text-white">
+              <ThermometerSnowflake className="h-12 w-12 mx-4"/> 25°C
+            </div>
+            <div className="flex flex-grow justify-center items-center text-6xl text-white">
+              <Droplets className="h-12 w-12 mx-4"/> 50%
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="flex h-2/6">
+        <Card className="flex flex-col bg-customDark w-1/4 m-2">
+          <CardHeader className="text-white">
+            <CardTitle>One hour</CardTitle>
+            <CardDescription className="text-white">Data from one hour ago</CardDescription>
+          </CardHeader>
+          <CardContent className="flex felx-row flex-grow">
+          <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              25°C
+            </div>
+            <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              50%
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col bg-customDark w-1/4 m-2">
+          <CardHeader className="text-white">
+            <CardTitle>Difference</CardTitle>
+            <CardDescription className="text-white">Difference from previous data</CardDescription>
+          </CardHeader>
+          <CardContent className="flex felx-row flex-grow">
+          <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              -5°C
+            </div>
+            <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              -2%
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col bg-customDark w-1/4 m-2">
+          <CardHeader className="text-white">
+            <CardTitle>One hour</CardTitle>
+            <CardDescription className="text-white">Data from one hour ago</CardDescription>
+          </CardHeader>
+          <CardContent className="flex felx-row flex-grow">
+          <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              25°C
+            </div>
+            <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              50%
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col bg-customDark w-1/4 m-2">
+          <CardHeader className="text-white">
+            <CardTitle>Difference</CardTitle>
+            <CardDescription className="text-white">Difference from previous data</CardDescription>
+          </CardHeader>
+          <CardContent className="flex felx-row flex-grow">
+          <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              -5°C
+            </div>
+            <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              -2%
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="flex h-2/6">
+        <Card className="flex flex-col bg-customDark w-1/4 m-2">
+          <CardHeader className="text-white">
+            <CardTitle>Average(24 hour)</CardTitle>
+            <CardDescription className="text-white">Average temperature over the past 24 hours</CardDescription>
+          </CardHeader>
+          <CardContent className="flex felx-row flex-grow">
+          <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              25°C
+            </div>
+            <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              50%
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col bg-customDark w-1/4 m-2">
+          <CardHeader className="text-white">
+            <CardTitle>Average(1 week)</CardTitle>
+            <CardDescription className="text-white">Average temperature over the past 1 week</CardDescription>
+          </CardHeader>
+          <CardContent className="flex felx-row flex-grow">
+          <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              25°C
+            </div>
+            <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              50%
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col bg-customDark w-1/4 m-2">
+          <CardHeader className="text-white">
+            <CardTitle>Average(24 hour)</CardTitle>
+            <CardDescription className="text-white">Average temperature over the past 24 hours</CardDescription>
+          </CardHeader>
+          <CardContent className="flex felx-row flex-grow">
+          <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              25°C
+            </div>
+            <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              50%
+            </div>
+          </CardContent>
+        </Card>
+        <Card className="flex flex-col bg-customDark w-1/4 m-2">
+          <CardHeader className="text-white">
+            <CardTitle>Average(1 week)</CardTitle>
+            <CardDescription className="text-white">Average temperature over the past 1 week</CardDescription>
+          </CardHeader>
+          <CardContent className="flex felx-row flex-grow">
+          <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              25°C
+            </div>
+            <div className="flex flex-grow justify-center items-center text-4xl text-white">
+              50%
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
